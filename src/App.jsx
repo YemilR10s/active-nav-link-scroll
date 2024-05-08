07da38389289ec51b4503ef034dd1bf4c31307c1
YemilR10s <sigifredoyemil14@gmail.com>
2024-05-08 14:22:53 -0500

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="navContainer">
+      <ul>
+        <li>
+          <a href="#home" className='item active'>home</a>
+        </li>
+        <li>
+          <a href="#services" className='item'>services</a>
+        </li>
+        <li>
+          <a href="#galeri"className='item'>galeri</a>
+        </li>
+        <li>
+          <a href="#contact"className='item'>contact</a>
+        </li>
+      </ul>
+      </div>
+
+      <div className="home" id='home'>
+        <p>Home</p>
+      </div>
+      
+      <div className="services" id='services'>
+        <p>services</p>
+      </div>
+
+      <div className="galeri" id='galeri'>
+        <p>galeri</p>
+      </div>
+
+      <div className="contact" id='contact'>
+        <p>contact</p>
+      </div>
+    </>
+  )
+}
+
+export default App
